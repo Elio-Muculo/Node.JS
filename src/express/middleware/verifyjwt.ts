d912ad5379549358e0f4f7fbfb2bb4e3254d0fc6
Elliot <emuculo25@gmail.com>
2022-08-27 00:37:50 +0200

    middleware done

---
+++ src/express/middleware/verifyjwt.ts
@@ -0,0 +1,24 @@
+const jwT = require("jsonwebtoken");
+require("dotenv").config();
+
+
+const verifyJwt = (req, res, next) => {
+    const authHeader = req.headers('authorization');
+
+    if (!authHeader) return res.sendStatus(401);
+    console.log(authHeader); // bearer token
+
+    const token = authHeader.split(' ')[1];
+    jwT.verify(
+        token,
+        process.env.ACESS_TOKEN_SECRET, 
+        (err, decoded) => {
+            if (err) return res.sendStatus(403);
+            req.user = decoded.username;
+            next();
+        }
+    )
+    
+}
+
+module.exports = { verifyJwt };
